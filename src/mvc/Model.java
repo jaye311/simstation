4bab90fa0cc7e5c420e974d5e2f2da534c1a66d8
jaye311 <johnathanaye311@gmail.com>
2025-03-07 10:43:33 -0800

    Changed to Java 17 mvcApps version 0. Implemented Subscriber and Publisher from TurtleGraphics. Programmed abstract class Model.

--- src/mvc/Model.java
+++ src/mvc/Model.java
@@ -1,4 +1,23 @@
 package mvc;
 
-public class Model {
+import java.io.Serializable;
+
+public abstract class Model extends Publisher implements Serializable {
+    boolean unsavedChanges = false;
+    String fileName = null;
+    public void setUnsavedChanges(boolean b){
+        unsavedChanges = b;
+    }
+    public boolean getUnsavedChanges(){
+        return unsavedChanges;
+    }
+    public void changed(){
+        unsavedChanges = true;
+    }
+    public String getFileName(){
+        return fileName;
+    }
+    public void setFileName(String s){
+        fileName = s;
+    }
 }
--- src/stopLight/ChangeCommand.java
+++ src/stopLight/ChangeCommand.java
@@ -1,4 +1,16 @@
 package stopLight;
 
-public class ChangeCommand {
+import mvc.*;
+
+public class ChangeCommand extends Command {
+
+    public ChangeCommand(Model model) {
+        super(model);
+    }
+
+    public void execute() {
+        Stoplight light = (Stoplight)model;
+        light.change();
+    }
+
 }
