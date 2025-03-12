20794639da720da7571d7e437a5d6e4cb6ae7b0b
jaye311 <johnathanaye311@gmail.com>
2025-03-12 14:10:16 -0700

    made model extension MineField, is a 2d array of Cell which implements Serializable, MineFieldTest uses a text representation, seems to work

--- src/mvc/Model.java
+++ src/mvc/Model.java
@@ -9,16 +9,20 @@ public abstract class Model extends Publisher implements Serializable {
     public void setUnsavedChanges(boolean b){
         unsavedChanges = b;
     }
+
     public boolean getUnsavedChanges(){
         return unsavedChanges;
     }
+
     public void changed(){
         unsavedChanges = true;
         notifySubscribers();
     }
+
     public String getFileName(){
         return fileName;
     }
+
     public void setFileName(String s){
         fileName = s;
     }
