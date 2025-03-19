4855a2d4b831036c5344acf8a93882fbce252e7c
jaye311 <johnathanaye311@gmail.com>
2025-03-18 21:42:48 -0700

    debugged MineField

--- src/mineField/MineFieldException.java
+++ src/mineField/MineFieldException.java
@@ -1,7 +1,12 @@
 package mineField;
 
 public class MineFieldException extends Exception{
+    boolean firstWin;
     public MineFieldException(String s){
+        this(s, false);
+    }
+    public MineFieldException(String s, boolean b){
         super(s);
+        firstWin = b;
     }
 }
